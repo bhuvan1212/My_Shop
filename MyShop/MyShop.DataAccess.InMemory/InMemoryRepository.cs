fe5001f3245558deee83d3e00a95486469320358
bhuvan1212 <64746240+bhuvan1212@users.noreply.github.com>
2020-05-09 23:50:17 +0400

    Implemented dependency Injection

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T:BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
@@ -39,7 +40,7 @@ namespace MyShop.DataAccess.InMemory
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
 
-            if (tToUpdate!=null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -51,9 +52,9 @@ namespace MyShop.DataAccess.InMemory
 
         public T Find(string Id)
         {
-            T tToFind = items.Find(i=>i.Id==Id);
+            T tToFind = items.Find(i => i.Id == Id);
 
-            if (tToFind!=null)
+            if (tToFind != null)
             {
                 return tToFind;
             }
