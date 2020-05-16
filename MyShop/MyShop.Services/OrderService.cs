94e15d594adc64e0ca25a12038dce338e755ab13
bhuvan1212 <64746240+bhuvan1212@users.noreply.github.com>
2020-05-17 00:19:17 +0400

    Added Creation Order

---
+++ MyShop/MyShop.Services/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Services
+{
+    public class OrderService:IOrderService
+    {
+        IRepository<Order> orderContext;
+
+        public OrderService(IRepository<Order> orderContext)
+        {
+            this.orderContext = orderContext;
+        }
+
+        public void CreateOrder(Order basedOrder, List<BasketItemViewModel> basketItems)
+        {
+            foreach (var item in basketItems)
+            {
+                basedOrder.OrderItems.Add(new OrderItem()
+                {
+                    ProductId = item.Id,
+                    Image = item.Image,
+                    Price = item.Price,
+                    ProductName = item.ProductName,
+                    Quantity = item.Quantity
+                }
+                );
+            }
+
+            orderContext.Insert(basedOrder);
+            orderContext.Commit();
+        }
+    }
+}
